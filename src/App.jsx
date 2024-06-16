43d651ce493ef0c7151e5eb6298be690ad97150e
Aryan Thadani <aryanthadani0@gmail.com>
2024-06-16 15:48:52 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Gallery from "./components/Gallery";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Infinite Scrolling Gallery</h1>
+      <Gallery />
+    </div>
+  );
+};
+
+export default App;
